7f4013eac96cbbf95afebe7ce1964f567554ee72
Stefan Zabrian <faniss2005@yahoo.com>
2024-01-09 14:31:30 +0200

    Added SecurityConfig -> @Bean filterChain

---
+++ src/main/java/com/picture/shop/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.picture.shop.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebSecurity
+@EnableWebMvc
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( auth ->
+                        auth
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
