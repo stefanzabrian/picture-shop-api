b350e1adc0d0bf7ef34fce12b4b56ba4544de9dc
Stefan Zabrian <faniss2005@yahoo.com>
2024-01-27 14:14:33 +0200

    Created view-client-details api and tested

--- src/main/java/com/picture/shop/controller/UserController.java
+++ src/main/java/com/picture/shop/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.picture.shop.controller;
 
 import com.picture.shop.controller.exception.JwtValidationException;
+import com.picture.shop.model.Client;
+import com.picture.shop.model.User;
 import com.picture.shop.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -28,5 +30,16 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
+    @GetMapping("/view-client-details")
+    public ResponseEntity<?> viewClientDetails(Principal principal){
+        String email = principal.getName();
+        Optional<User> user = userService.findByEmail(email);
+        if (user.isEmpty()) {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User with email: " + email + " not found!");
+        }
+        Client client = user.get().getClient();
+        return ResponseEntity.status(HttpStatus.OK).body(client);
+    }
+
 
 }
