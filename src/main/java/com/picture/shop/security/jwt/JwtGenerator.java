0913576af95fb7eac5a46814d04fe8d7569742c1
Stefan Zabrian <faniss2005@yahoo.com>
2024-02-12 18:47:09 +0200

    Added claims authorities in the accessToken

--- src/main/java/com/picture/shop/security/jwt/JwtGenerator.java
+++ src/main/java/com/picture/shop/security/jwt/JwtGenerator.java
@@ -5,8 +5,10 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.Date;
 
 @Component
@@ -38,11 +40,13 @@ public class JwtGenerator {
     }
     public String generateToken(Authentication authentication) {
         String email = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         Date currentDate = new Date();
         Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
 
         String token = Jwts.builder()
                 .setSubject(email)
+                .claim("authorities", authorities)
                 .setIssuedAt(currentDate)
                 .setExpiration(expireDate)
                 .signWith(SignatureAlgorithm.HS512, SecurityConstants.JWT_SECRET)
